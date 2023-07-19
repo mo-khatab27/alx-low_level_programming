7fff88052d59ae59d5889008dd87a47f2743642a
mo-khatab27 <fathyn499@gmail.com>
2023-07-19 13:53:30 -0700

    1-alphabet.c_khatab

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,14 @@
 #include "main.h"
+/**
+ * print_alphabet - printing lower case
+ * Return: return 0
+ */
 void print_alphabet(void)
 {
-     char f;
-     for(f='a';f<='z';f++)
-     {
-	     _putchar(f);
-             _putchar('\n');
-
-     }
+	char f;
+	for (f = 'a' ; f <= 'z' ; f++)
+	{
+		_putchar(f);
+		_putchar('\n');
+	}
 }
