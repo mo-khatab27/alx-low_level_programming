c0e890fa7d8a3fe56a855cede93fc4347a0d9655
mo-khatab27 <fathyn499@gmail.com>
2023-07-21 08:16:29 -0700

    khatab_ isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - func to check if the char is uppercase or not
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+khatab_0x04-more_functions_nested_loops
