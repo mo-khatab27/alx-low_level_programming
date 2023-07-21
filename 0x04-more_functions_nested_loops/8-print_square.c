1b299b599b144d438892466ac067d5cd59a689a0
mo-khatab27 <fathyn499@gmail.com>
2023-07-21 15:24:05 -0700

    khatab_task-7-8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - func
+ * @size: times
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i < size ; i++)
+	{
+		for (j = 0 ; j < size ; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
