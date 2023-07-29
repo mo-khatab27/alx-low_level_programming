847b7a2819c4ef12a0110c6180a2231ab9b172da
mo-khatab27 <fathyn499@gmail.com>
2023-07-29 04:17:00 -0700

    khatab_ 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
- * rev_string - function
- * @s: string
+ * rev_string - function reverses a word
+ * @s: string to be reversed
  * Return: void
  */
 void rev_string(char *s)
 {
-	int i;
-	char p[10];
+int i;
+char j;
+int count = 0;
 
-	for (i = 0  ; i < 9 ; i++)
-	{
-		p[i] = s[8 - i];
-	}
-	for (i = 0 ; i < 9 ; i++)
-	{
-		s[i] = p[i];
-	}
+for (i = 0 ; s[i] != '\0' ; i++)
+	count++;
+for (i = 0; i < count / 2; i++)
+{
+	j = s[i];
+	s[i] = s[count - 1 - i];
+	s[count - 1 - i] = j;
+}
 }
